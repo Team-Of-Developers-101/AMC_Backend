87963e8c7cb2a8524bdc95d0440db42388d0227e
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-04-29 10:00:00 +0100

    refactor: merge permutation functions into one

--- calculator/v1/perm_comb.py
+++ calculator/v1/perm_comb.py
@@ -5,7 +5,8 @@ from itertools import permutations
 from typing import List, Union
 
 
-def permutation(objects: Union[int, List], sample: int = None) -> List:
+def permutation(
+        objects: Union[int, List], sample: int = None) -> Union[int, List]:
     """Get all permutations of lists"""
     if not isinstance(objects, int) and not sample:
         sample = len(objects)
@@ -18,36 +19,17 @@ def permutation(objects: Union[int, List], sample: int = None) -> List:
     for i in list(perm):
         result.append(i)
         # print (i)
-    return len(result)
+    return len(result), result
 
 
-def permutation_visuals(objects: Union[int, List], sample: int = None) -> List:
-    """See all permutations of lists"""
-    if not isinstance(objects, int) and not sample:
-        sample = len(objects)
-    elif not sample:
-        sample = objects
-    if isinstance(objects, int):
-        objects = range(objects)
-    perm = permutations(objects, sample)
-    result = []
-    for i in list(perm):
-        result.append(i)
-    return result
+test_list = ["a", "w", "s", "d", "f"]
 
+rez, rez_list = permutation(5, 2)
+print(rez)
+print(rez_list)
 
-# test_list = ["a", "w", "s", "d", "f"]
+print(f"{'='*80}")
 
-# rez = permutation_visuals(5, 2)
-# print(rez)
-
-# rez = permutation(5, 2)
-# print(rez)
-
-# print(f"{'='*80}")
-
-# rez = permutation_visuals(test_list, 2)
-# print(rez)
-
-# rez = permutation(test_list, 2)
-# print(rez)
+rez, rez_list = permutation(test_list, 2)
+print(rez)
+print(rez_list)
